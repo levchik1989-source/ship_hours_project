4b0b036ece57d1a69b6c32b34202c2d33fe0a823
root <root@localhost.localdomain>
2026-07-08 00:04:29 +0000

    Fix Non-UK slot persistence

--- lib/data/hive/adapters/work_slot_hive_model.dart
+++ lib/data/hive/adapters/work_slot_hive_model.dart
@@ -2,24 +2,33 @@ import '../../../core/enums/work_slot_state.dart';
 import '../../../domain/entities/work_slot.dart';
 
 final class WorkSlotHiveModel {
-  const WorkSlotHiveModel({required this.index, required this.state});
+  const WorkSlotHiveModel(
+      {required this.index, required this.state, this.isUkWaters = true});
 
   factory WorkSlotHiveModel.fromEntity(WorkSlot slot) {
-    return WorkSlotHiveModel(index: slot.index, state: slot.state.name);
+    return WorkSlotHiveModel(
+        index: slot.index, state: slot.state.name, isUkWaters: slot.isUkWaters);
   }
 
   factory WorkSlotHiveModel.fromMap(Map<dynamic, dynamic> map) {
-    return WorkSlotHiveModel(index: map['index'] as int, state: map['state'] as String);
+    return WorkSlotHiveModel(
+        index: map['index'] as int,
+        state: map['state'] as String,
+        isUkWaters: map['isUkWaters'] as bool? ?? true);
   }
 
   final int index;
   final String state;
+  final bool isUkWaters;
 
   WorkSlot toEntity() {
-    return WorkSlot(index: index, state: WorkSlotState.fromName(state));
+    return WorkSlot(
+        index: index,
+        state: WorkSlotState.fromName(state),
+        isUkWaters: isUkWaters);
   }
 
   Map<String, dynamic> toMap() {
-    return {'index': index, 'state': state};
+    return {'index': index, 'state': state, 'isUkWaters': isUkWaters};
   }
 }
